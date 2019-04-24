cd9ee2119fd083ac68c958f6086532d8ed529fd8
Aaron Raff <aaronraffdev@gmail.com>
2019-04-23 22:07:16 -0400

    Initial commit

---
+++ src/main/java/WordCount.java
@@ -0,0 +1,53 @@
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.*;
+
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+public class WordCount {
+
+    public static void main(String[] args) throws Exception {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-words");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        final StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> source = builder.stream("streams-plaintext-input");
+        KTable<String, Long> counts = source.groupBy(new KeyValueMapper<String, String, String>() {
+            public String apply(String key, String val) {
+                return val;
+            }
+        }).count();
+
+        counts.toStream().to("streams-counts-output", Produced.with(Serdes.String(), Serdes.Long()));
+
+        final Topology topology = builder.build();
+        System.out.println(topology.describe());
+
+        final KafkaStreams streams = new KafkaStreams(topology, props);
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
+            @Override
+            public void run() {
+                streams.close();
+                latch.countDown();
+            }
+        });
+
+        try {
+            streams.start();
+            latch.await();
+        } catch (Throwable e) {
+            System.exit(1);
+        }
+
+        System.exit(0);
+    }
+}
